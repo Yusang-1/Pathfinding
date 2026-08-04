10fb2b446a9ff829e342e172726a8ded06d0329e
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2026-08-04 16:02:51 +0900

    Chore : 중복되는 스크립트 오브젝트 제거

---
+++ Assets/Scripts/SO/SteeringWeightingSO.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "SteeringWeightingSO", menuName = "Scriptable Objects/CrowdSimulation/SteeringWeightingSO")]
+public class SteeringWeightingSO : ScriptableObject
+{
+    public SteeringConfig WalkConfig;
+}
+
+[Serializable]
+public struct SteeringConfig
+{
+    public float SeekWeight;
+    public float SeparationWeight;
+    public float CohesionWeight;
+    public float AlignmentWeight;
+}
