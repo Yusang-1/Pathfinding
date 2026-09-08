96431a4f7be075111604b5ee343354046ff15916
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2026-09-08 16:22:43 +0900

    Chore : chore

--- Assets/Scripts/ECSControllUnit/ECSPathfindingBridge.cs
+++ Assets/Scripts/ECSControllUnit/ECSPathfindingBridge.cs
@@ -261,7 +261,7 @@ namespace Assets.Scripts.ECSControllUnit
 
         private ClusterResultWrapper GetAbstractPath(Vector3 from, Vector3 to, float unitRadius)
         {
-            clusterResultWrapper.Reset();
+            clusterResultWrapper.ResetClusterResult();
             clusterResultWrapper.SetStart(from, to, unitRadius);
 
             var clusterPath = highLevelPathfinder.FindClusterPath(clusterResultWrapper);
