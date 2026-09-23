00d8edabe9891fb9187e9f819987e10456486783
Yusang-1 <206505633+Yusang-1@users.noreply.github.com>
2026-09-23 19:42:43 +0900

    Fix : drag ui의 위치가 어긋나던 오류 수정

--- Assets/Scripts/ControllUnit/UI/UIDragController.cs
+++ Assets/Scripts/ControllUnit/UI/UIDragController.cs
@@ -7,16 +7,22 @@ namespace Assets.Scripts.ControllUnit.UI
     {
         public event Action<Vector3, float, float> OnFindUnitsInDragUI;
 
+        [SerializeField] private Canvas canvas;
         [SerializeField] private GameObject dragUI;
 
+        private RectTransform canvasRect;
         private RectTransform dragUIRect;
 
         private Vector3 standardPosition;
+        private Vector3 canvasStandardPosition;
         private Vector3 compareSizeDelta;
 
         private void Start()
         {
+            canvas = canvas != null ? canvas : FindAnyObjectByType<Canvas>();
+
             dragUIRect = dragUI.GetComponent<RectTransform>();
+            canvasRect = canvas.GetComponent<RectTransform>();
         }
 
         public void DragStarted(Vector3 startPosition)
@@ -24,25 +30,34 @@ namespace Assets.Scripts.ControllUnit.UI
             dragUI.SetActive(true);
 
             standardPosition = startPosition;
-            dragUIRect.anchoredPosition = standardPosition;
+            canvasStandardPosition = ScreenToCanvasPosition(startPosition);
+            
+            dragUIRect.anchoredPosition = canvasStandardPosition;
         }
 
         private Vector2 dragUISizeDelta;
         public void DragPerformed(Vector3 position)
         {
-            dragUISizeDelta.x = standardPosition.x - position.x;
-            dragUISizeDelta.y = standardPosition.y - position.y;
-            compareSizeDelta = dragUISizeDelta;
-
-            DrawDragUI(position);
+            var canvasPosition = ScreenToCanvasPosition(position);
             
-            OnFindUnitsInDragUI?.Invoke(standardPosition, compareSizeDelta.x, compareSizeDelta.y);
+            dragUISizeDelta = new Vector2(
+                canvasStandardPosition.x - canvasPosition.x,
+                canvasStandardPosition.y - canvasPosition.y
+            );
+            compareSizeDelta = new Vector2(
+                standardPosition.x - position.x,
+                standardPosition.y - position.y
+            );
+
+            DrawDragUI(canvasPosition);
+
+            OnFindUnitsInDragUI?.Invoke(standardPosition, compareSizeDelta.x, compareSizeDelta.y); // screen좌표 기준 전달
         }
         public Vector3 ECSDragPerformed(Vector3 position)
         {
             Camera cam = Camera.main;
             float depth = cam.orthographic ? 0f : Mathf.Abs(cam.transform.position.z);
-            Vector3 worldPosition = cam.ScreenToWorldPoint(new Vector3(position.x, position.y, depth));            
+            Vector3 worldPosition = cam.ScreenToWorldPoint(new Vector3(position.x, position.y, depth));
 
             dragUISizeDelta.x = standardPosition.x - position.x;
             dragUISizeDelta.y = standardPosition.y - position.y;
@@ -54,32 +69,32 @@ namespace Assets.Scripts.ControllUnit.UI
 
         private void DrawDragUI(Vector3 position)
         {
-            int x = standardPosition.x < position.x ? 1 : -1;
-            int y = standardPosition.y < position.y ? 1 : -1;
+            int x = canvasStandardPosition.x < position.x ? 1 : -1;
+            int y = canvasStandardPosition.y < position.y ? 1 : -1;
 
             if (x > 0 && y > 0) // 1사분면
             {
                 dragUIRect.pivot = new Vector2(0, 0);
-                compareSizeDelta.x = Mathf.Abs(dragUISizeDelta.x);
-                compareSizeDelta.y = Mathf.Abs(dragUISizeDelta.y);
+                compareSizeDelta.x = Mathf.Abs(compareSizeDelta.x);
+                compareSizeDelta.y = Mathf.Abs(compareSizeDelta.y);
             }
             else if (x < 0 && y > 0) // 2사분면
             {
                 dragUIRect.pivot = new Vector2(1, 0);
-                compareSizeDelta.x = -Mathf.Abs(dragUISizeDelta.x);
-                compareSizeDelta.y = Mathf.Abs(dragUISizeDelta.y);
+                compareSizeDelta.x = -Mathf.Abs(compareSizeDelta.x);
+                compareSizeDelta.y = Mathf.Abs(compareSizeDelta.y);
             }
             else if (x < 0 && y < 0)  // 3사분면
             {
                 dragUIRect.pivot = new Vector2(1, 1);
-                compareSizeDelta.x = -Mathf.Abs(dragUISizeDelta.x);
-                compareSizeDelta.y = -Mathf.Abs(dragUISizeDelta.y);
+                compareSizeDelta.x = -Mathf.Abs(compareSizeDelta.x);
+                compareSizeDelta.y = -Mathf.Abs(compareSizeDelta.y);
             }
             else if (x > 0 && y < 0) // 4사분면
             {
                 dragUIRect.pivot = new Vector2(0, 1);
-                compareSizeDelta.x = Mathf.Abs(dragUISizeDelta.x);
-                compareSizeDelta.y = -Mathf.Abs(dragUISizeDelta.y);
+                compareSizeDelta.x = Mathf.Abs(compareSizeDelta.x);
+                compareSizeDelta.y = -Mathf.Abs(compareSizeDelta.y);
             }
 
             dragUIRect.sizeDelta = new Vector2(Mathf.Abs(dragUISizeDelta.x), Mathf.Abs(dragUISizeDelta.y));
@@ -88,6 +103,25 @@ namespace Assets.Scripts.ControllUnit.UI
         public void DragCanceled()
         {
             dragUI.SetActive(false);
+        }
+
+        private Vector2 ScreenToCanvasPosition(Vector2 screenPosition)
+        {                        
+            Camera eventCamera = canvas.renderMode == RenderMode.ScreenSpaceOverlay
+                ? null
+                : canvas.worldCamera
+            ;
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                canvasRect,
+                screenPosition,
+                eventCamera,
+                out Vector2 localPosition
+            );
+            
+            Debug.Log($"{Camera.main.ScreenToViewportPoint(screenPosition)}\n{localPosition}");
+
+            return localPosition;
         }
     }
 }
